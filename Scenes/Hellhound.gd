60800dc8cc80e9b6686060ba057bd5cfcb7acb56
LucasOyarzun <lkzoyarzun@gmail.com>
2021-04-09 22:46:12 -0400

    Hell Hound move ready

---
+++ Scenes/Hellhound.gd
@@ -0,0 +1,57 @@
+extends KinematicBody2D
+
+var lineal_vel = Vector2()
+var speed_x = 100
+var speed_y = 500
+var gravity = 20
+
+var facing_right = false
+var waiting_before_turn_back = 2.5
+
+onready var playback = $AnimationTree.get("parameters/playback")
+var target_vel = -1
+var moving = false
+var timer_moving = 0
+	
+func _physics_process(delta: float) -> void:
+	
+	lineal_vel = move_and_slide(lineal_vel, Vector2.UP)
+	lineal_vel.y += gravity
+	waiting_before_turn_back-=delta
+	timer_moving+=delta
+	
+	if is_on_floor():
+		lineal_vel.x = lerp(lineal_vel.x, target_vel * speed_x, 0.5)
+	else:
+		lineal_vel.x = lerp(lineal_vel.x, target_vel * speed_x, 0.1)
+		
+	if not(moving):
+		lineal_vel.x = 0
+		
+	if(timer_moving>1.2 and moving == false):
+		moving = true
+		timer_moving = 0
+	elif(timer_moving>4 and moving ==true):
+		moving = false
+		timer_moving = 0
+		
+		
+	# Para que de vueltas en la plataforma
+
+	if $RayCast2D.is_colliding():
+		pass
+	else:
+		if(waiting_before_turn_back<0):
+			waiting_before_turn_back= 1
+			print("hola")
+			facing_right = !(facing_right)
+			scale.x = -1
+			target_vel = -target_vel
+	
+	# Animations
+	if is_on_floor():
+		if abs(lineal_vel.x) > 10:
+			playback.travel("Walk")
+		else:
+			playback.travel("Idle")
+	
