de29b9d326b5ad3c2d47e0bc3e470c1489a34bc9
LucasOyarzun <lkzoyarzun@gmail.com>
2021-07-25 22:11:46 -0400

    Escena del inicio casi casi lista

--- Scenes/Intro_scene.gd
+++ Scenes/Intro_scene.gd
@@ -14,3 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Intro_animation_finished(anim_name):
+	get_tree().change_scene("res://Scenes/Level1_Tutorial.tscn")
