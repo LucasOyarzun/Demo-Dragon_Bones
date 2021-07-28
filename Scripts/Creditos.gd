f9e045675615ccc5621309527ccf58018770ac9d
LucasOyarzun <lkzoyarzun@gmail.com>
2021-07-27 22:12:05 -0400

    Creditos listos

---
+++ Scripts/Creditos.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Transition.play("Up")
+	
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		skip_scene()
+		
+func skip_scene():
+	get_tree().change_scene("res://Scenes/Menu.tscn")
